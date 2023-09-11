c42e34d23bc44abb497a7f7f1062ff973513c136
Sanchay Kumar <sanchaykumar9@gmail.com>
2023-09-11 20:01:26 +0530

    use fa instead of remix

--- js/Theme.js
+++ js/Theme.js
@@ -1,34 +1,25 @@
-function toggleMode(newMode) {
-    const root = document.documentElement;
-    if (newMode === 'dark') {
-        root.style.setProperty('--fgColor', 'var(--dark-fgColor)');
-        root.style.setProperty('--bgColor', 'var(--dark-bgColor)');
-        root.style.setProperty('--bgFilter', 'var(--dark-bgFilter)');
-        root.style.setProperty('--bgImage', 'var(--dark-bgImage)');
-    } else {
-        root.style.setProperty('--fgColor', 'var(--light-fgColor)');
-        root.style.setProperty('--bgColor', 'var(--light-bgColor)');
-        root.style.setProperty('--bgFilter', 'var(--light-bgFilter)');
-        root.style.setProperty('--bgImage', 'var(--light-bgImage)');
-    }
+function setMode(isDark) {
+  const root = document.documentElement;
 
-    // Store the selected mode in localStorage
-    localStorage.setItem('mode', newMode);
+  if (isDark) {
+    root.style.setProperty("--fgColor", "var(--dark-fgColor)");
+    root.style.setProperty("--bgColor", "var(--dark-bgColor)");
+    root.style.setProperty("--bgFilter", "var(--dark-bgFilter)");
+    root.style.setProperty("--bgImage", "var(--dark-bgImage)");
+  } else {
+    root.style.setProperty("--fgColor", "var(--light-fgColor)");
+    root.style.setProperty("--bgColor", "var(--light-bgColor)");
+    root.style.setProperty("--bgFilter", "var(--light-bgFilter)");
+    root.style.setProperty("--bgImage", "var(--light-bgImage)");
+  }
 }
 
-function detectAndToggleMode() {
-    // Detect user preference for dark mode
-    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+// Set initial mode
+setMode(window.matchMedia("(prefers-color-scheme: dark)").matches);
 
-    // Set the initial mode based on user preference or stored preference
-    const initialMode = localStorage.getItem('mode') || (prefersDarkMode ? 'dark' : 'light');
-    toggleMode(initialMode);
-
-    // Watch for changes in system color scheme preference
-    window.matchMedia('(prefers-color-scheme: dark)').addListener(event => {
-        toggleMode(event.matches ? 'dark' : 'light');
-    });
-}
-
-// Call the function to detect and toggle the mode
-detectAndToggleMode();
+// Listen for changes in system color scheme preference
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
+    setMode(event.matches);
+  });
--- js/setLinks.js
+++ js/setLinks.js
@@ -1,29 +1,68 @@
+const FA_ICONS = [
+  {
+    icon: "fa-github",
+    link: "https://www.github.com/",
+  },
+  {
+    icon: "fa-steam",
+    link: "https://store.steampowered.com/",
+  },
+  {
+    icon: "fa-unsplash",
+    link: "https://unsplash.com/",
+  },
+  {
+    icon: "fa-reddit",
+    link: "https://www.reddit.com/",
+  },
+];
+
+const LIST_ITEMS = [
+  {
+    name: "clist",
+    link: "https://clist.by/",
+  },
+  {
+    name: "goodreads",
+    link: "https://www.goodreads.com/",
+  },
+  {
+    name: "lichess",
+    link: "https://lichess.org/",
+  },
+  {
+    name: "hackernews",
+    link: "https://news.ycombinator.com/",
+  },
+];
+
 const printIcons = () => {
-    for (const card of ICONS) {
-        let currentItem = document.createElement("a");
-        let currentItemIcon = document.createElement("span");
+  for (const card of FA_ICONS) {
+    // font awesome icons
+    let currentItem = document.createElement("a");
+    let currentItemIcon = document.createElement("i");
 
-        currentItem.classList.add("icon");
-        currentItem.href = card.link;
+    currentItem.classList.add("icon");
+    currentItem.href = card.link;
 
-        currentItemIcon.classList.add("fas");
-        currentItemIcon.classList.add(card.icon);
+    currentItemIcon.classList.add("fab");
+    currentItemIcon.classList.add(card.icon);
 
-        currentItem.append(currentItemIcon);
-        iconsContainer.appendChild(currentItem);
-    }
+    currentItem.append(currentItemIcon);
+    iconsContainer.appendChild(currentItem);
+  }
 };
 
 const printListItems = () => {
-    for (const listItem of LIST_ITEMS) {
-        let currentItem = document.createElement("a");
+  for (const listItem of LIST_ITEMS) {
+    let currentItem = document.createElement("a");
 
-        currentItem.classList.add("listItem");
-        currentItem.href = listItem.link;
+    currentItem.classList.add("listItem");
+    currentItem.href = listItem.link;
 
-        currentItem.appendChild(document.createTextNode(listItem.name));
-        listContainer.appendChild(currentItem);
-    }
+    currentItem.appendChild(document.createTextNode(listItem.name));
+    listContainer.appendChild(currentItem);
+  }
 };
 
 printIcons();
